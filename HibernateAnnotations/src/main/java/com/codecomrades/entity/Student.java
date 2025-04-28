7517cfec6a0ce742b322c041d67eb12d29a7efab
somjena <jenasoumitrapvt@gmail.com>
2025-04-29 01:01:03 +0530

    Student the Entity Class

---
+++ HibernateAnnotations/src/main/java/com/codecomrades/entity/Student.java
@@ -0,0 +1,45 @@
+package com.codecomrades.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    private int sid;
+    private String sname;
+    private int sage;
+    private int mark;
+
+    public int getSid() {
+        return sid;
+    }
+
+    public void setSid(int sid) {
+        this.sid = sid;
+    }
+
+    public String getSname() {
+        return sname;
+    }
+
+    public void setSname(String sname) {
+        this.sname = sname;
+    }
+
+    public int getSage() {
+        return sage;
+    }
+
+    public void setSage(int sage) {
+        this.sage = sage;
+    }
+
+    public int getMark() {
+        return mark;
+    }
+
+    public void setMark(int mark) {
+        this.mark = mark;
+    }
+}
