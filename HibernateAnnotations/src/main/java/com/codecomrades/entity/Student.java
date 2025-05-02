9b2e464d7b0bf6b02893569fc53ed5499bded596
somjena <jenasoumitrapvt@gmail.com>
2025-05-02 16:48:16 +0530

    Student .java

--- HibernateAnnotations/src/main/java/com/codecomrades/entity/Student.java
+++ HibernateAnnotations/src/main/java/com/codecomrades/entity/Student.java
@@ -1,10 +1,13 @@
 package com.codecomrades.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Student {
+
     @Id
     private int sid;
     private String sname;
