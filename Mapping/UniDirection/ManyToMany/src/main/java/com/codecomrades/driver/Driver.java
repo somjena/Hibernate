2b58b06f7b72f7e20b7c43a63dd469c67981c6b3
somjena <jenasoumitrapvt@gmail.com>
2025-05-15 20:48:04 +0530

    Add All the Methods We want to Perform

--- Mapping/UniDirection/ManyToMany/src/main/java/com/codecomrades/driver/Driver.java
+++ Mapping/UniDirection/ManyToMany/src/main/java/com/codecomrades/driver/Driver.java
@@ -16,7 +16,42 @@ public class Driver {
         emf= Persistence.createEntityManagerFactory("ManyToMany");
         em= emf.createEntityManager();
         et= em.getTransaction();
+        sc=new Scanner(System.in);
     }
     public static void main(String[] args) {
+        System.out.println("Welcome to Many To Many Mapping Example::__::");
+        boolean flag = true;
+        while(flag){
+            System.out.println("Enter the Option U want to Perform");
+            System.out.println("1.Add Student\n2.Add Subject\n3.Add SubjectForStudent\n4.Delete SubjectforStudent\n5.Update Student\n6.Update Subject\n7.Exit");
+            int choice = sc.nextInt();
+            switch (choice){
+                case 1:addstudent();
+                break;
+                case 2:addSubject();
+                break;
+                case 3:AddSubjectforStudent();
+                break;
+                case 4:DeleteSubjectForStudent();
+                break;
+                case 5:UpdateStudent();
+                
+            }
+        }
+    }
+
+    private static void UpdateStudent() {
+    }
+
+    private static void DeleteSubjectForStudent() {
+    }
+
+    private static void AddSubjectforStudent() {
+    }
+
+    private static void addSubject() {
+    }
+
+    private static void addstudent() {
     }
 }
