d56ba6996bffaaf38994d71471ce1ddf915a567a
somjena <jenasoumitrapvt@gmail.com>
2025-05-15 20:37:59 +0530

    Creating the Methods

--- Mapping/UniDirection/ManyToMany/src/main/java/com/codecomrades/driver/Driver.java
+++ Mapping/UniDirection/ManyToMany/src/main/java/com/codecomrades/driver/Driver.java
@@ -1,7 +1,22 @@
 package com.codecomrades.driver;
 
-public class Driver {
-    public static void main(String[] args) {
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
 
+import java.util.Scanner;
+
+public class Driver {
+    private static EntityManagerFactory emf;
+    private static EntityManager em;
+    private static EntityTransaction et;
+    private static Scanner sc ;
+    static {
+        emf= Persistence.createEntityManagerFactory("ManyToMany");
+        em= emf.createEntityManager();
+        et= em.getTransaction();
+    }
+    public static void main(String[] args) {
     }
 }
