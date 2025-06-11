1bd38ba95b4f53564b88d57b0e654ab3dbea70b6
somjena <jenasoumitrapvt@gmail.com>
2025-06-11 22:39:17 +0530

    Engine Class Update

--- Mapping/BiDirection/OneToOne/src/main/java/com/codecomrades/Entity/Engine.java
+++ Mapping/BiDirection/OneToOne/src/main/java/com/codecomrades/Entity/Engine.java
@@ -1,4 +1,48 @@
 package com.codecomrades.Entity;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+
+@Entity
 public class Engine {
+    @Id
+    private int e_id;
+    private String e_type;
+    private int e_cc;
+    @OneToOne(cascade = CascadeType.ALL )
+    private Car car;
+
+    public Car getCar() {
+        return car;
+    }
+
+    public void setCar(Car car) {
+        this.car = car;
+    }
+
+    public int getE_id() {
+        return e_id;
+    }
+
+    public void setE_id(int e_id) {
+        this.e_id = e_id;
+    }
+
+    public String getE_type() {
+        return e_type;
+    }
+
+    public void setE_type(String e_type) {
+        this.e_type = e_type;
+    }
+
+    public int getE_cc() {
+        return e_cc;
+    }
+
+    public void setE_cc(int e_cc) {
+        this.e_cc = e_cc;
+    }
 }
