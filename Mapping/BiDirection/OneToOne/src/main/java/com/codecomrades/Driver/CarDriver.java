b0872f06ef8188912c3720edc7eba8ae51faba17
Soumitra jena <131749408+somjena@users.noreply.github.com>
2025-08-19 14:45:58 -0700

    Add stub methods for save, delete, and update actions
    Introduced private static methods save(), delete(), and Update() with placeholder print statements. Refactored main loop to prompt for user input and invoke corresponding methods based on selection.

--- Mapping/BiDirection/OneToOne/src/main/java/com/codecomrades/Driver/CarDriver.java
+++ Mapping/BiDirection/OneToOne/src/main/java/com/codecomrades/Driver/CarDriver.java
@@ -17,18 +17,39 @@ public class CarDriver {
        emf=Persistence.createEntityManagerFactory("OneToOneBi");
        em=emf.createEntityManager();
        et=em.getTransaction();
+       sc= new Scanner(System.in);
     }
 
     public static void main(String[] args) {
-        System.out.println("1.Save\n2.Delete\n3.Update\n4.SearchById\n5.findAll");
-        boolean choice = true;
-        while(true){
+
+        boolean option = true;
+        while(option){
+            System.out.println("1.Save\n2.Delete\n3.Update\n4.SearchById\n5.findAll");
+            int choice = sc.nextInt();
             switch (choice){
+                case 1:save();
+                break;
+                case 2:delete();
+                break;
+                case 3:Update();
+                break;
 
             }
         }
 
 
 
+    }
+
+    private static void Update() {
+        System.out.println("Update Method Invoked");
+    }
+
+    private static void delete() {
+        System.out.println("Delete Method Invoked");
+    }
+
+    private static void save() {
+        System.out.println("Save Method Invoked");
     }
 }
